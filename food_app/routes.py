5fd86fb713fa500dc7b983f2c2473e4345c04e16
olga <olga.gorbenko@gmail.com>
2021-06-22 14:03:22 +0300

    first login commit

--- food_app/routes.py
+++ food_app/routes.py
@@ -2,9 +2,24 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
-from food_app.forms import LoginForm
+from food_app.forms import LoginForm, RegistrationForm
 from food_app.models import User
-from .app_factory import app
+from .app_factory import app, db
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 
 @app.route('/')
