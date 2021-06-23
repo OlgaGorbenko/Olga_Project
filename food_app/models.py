d1db5161ced88ff0b127d07b111fe90fe2aaab4e
olga <olga.gorbenko@gmail.com>
2021-06-23 21:45:04 +0300

    new db

--- food_app/models.py
+++ food_app/models.py
@@ -29,45 +29,51 @@ def load_user(id):
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(256), index=True, unique=True)
-    type_of_product = db.Column(db.String(120), index=True)
-    unit_of_measure = db.Column(db.String(120), index=True)
+    type_of_product = db.Column(db.String(120))
+    unit_of_measure = db.Column(db.String(120))
 
     def __repr__(self):
         return '<Product {}>'.format(self.title)
 
 
-# class ShoppingList(db.Model):
-#     title = db.Column(db.String(256), index=True, unique=True, primary_key=True)  # pkey?
-#     owner = db.Column(db.String, db.ForeignKey('user.id'))
-#     items = db.relationship('ShoppingListItem') #backref='author', lazy='dynamic'
-#
-#     def __repr__(self):
-#         return '<Product {}>'.format(self.title)
+class ShoppingList(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256))
+    owner = db.Column(db.ForeignKey('user.id'))
+    items = db.relationship('ShoppingListItem', backref='shopping_list', lazy='dynamic')
 
-# class ShoppingListItem(db.Model):
-#     shopping_list = db.Column(db.String, db.ForeignKey('shopping_list.title'))  #?? # pkey?
-#     product = db.Column(db.String, db.ForeignKey('product.id'))
-#     quantity = db.Column(db.Integer, index=True)
-#     is_buyed = db.Column(db.Bool, index=True)
-#
-#     def __repr__(self):
-#         return '<Product {}>'.format(self.shopping_list)  #?
-#
-# class Recipe(db.Model):
-#     title = db.Column(db.String(256), index=True, unique=True, primary_key=True)  # pkey?
-#     ingredients = db.relationship('Ingredient') #backref='author', lazy='dynamic'
-#
-#     def __repr__(self):
-#         return '<Product {}>'.format(self.title)
-#
-# class Ingredient(db.Model):
-#     recipe = db.Column(db.String, db.ForeignKey('recipe.title'))  # pkey?
-#     product = db.Column(db.String, db.ForeignKey('product.id'))
-#     quantity = db.Column(db.Integer, index=True)
-#     unit_of_measure = db.Column(db.String(120), index=True)
-#
-#     def __repr__(self):
-#         return '<Product {}>'.format(self.recipe)  #?
+    def __repr__(self):
+        return '<ShoppingList {}>'.format(self.title)
+
+class ShoppingListItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    shopping_list_id = db.Column(db.ForeignKey('shopping_list.id'))
+    product_id = db.Column(db.ForeignKey('product.id'))
+    product = db.relationship('Product')
+    quantity = db.Column(db.Integer)
+    is_buyed = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return f'<ShoppingListItem {self.product.title}>'
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256), index=True, unique=True)
+    ingredients = db.relationship('Ingredient', backref='recipe', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Recipe {}>'.format(self.title)
+
+class Ingredient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_id = db.Column(db.ForeignKey('recipe.id'))
+    product = db.Column(db.ForeignKey('product.id'))
+    quantity = db.Column(db.Integer)
+    unit_of_measure = db.Column(db.String(120))
+
+    def __repr__(self):
+        return '<Ingredient {}>'.format(self.product)
+
 
 
 
