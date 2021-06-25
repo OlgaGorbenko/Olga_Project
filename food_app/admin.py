6e6f709d00c7c18618bb63b503c6abdb6aab861f
olga <olga.gorbenko@gmail.com>
2021-06-25 03:30:04 +0300

    flask-admin

--- food_app/admin.py
+++ food_app/admin.py
@@ -1,12 +1,38 @@
 from flask import url_for, request
 from flask_admin.contrib import sqla
 from werkzeug.utils import redirect
+from flask_admin.contrib.sqla import ModelView
 
-from food_app.models import Product
 from .app_factory import admin, db, login
+from food_app.models import User
+
+
+# class MicroBlogModelView(ModelView):
+#     can_delete = False  # disable model deletion
+#     page_size = 50  # the number of entries to display on the list view
 
 
 class MicroBlogModelView(sqla.ModelView):
+    can_delete = False  # disable model deletion
+    page_size = 50  # the number of entries to display on the list view
+    can_create = False
+    can_edit = False
+    can_view_details = True
+    column_exclude_list = ['password', ]
+    column_searchable_list = ['name', 'email']
+    column_filters = ['country']
+    column_editable_list = ['name', 'last_name']
+    create_modal = True
+    edit_modal = True
+    form_excluded_columns = ['last_name', 'email']
+    form_widget_args = {
+        'description': {
+            'rows': 10,
+            'style': 'color: black'
+        }
+    }
+    can_export = True
+
 
     def is_accessible(self):
         return login.current_user.is_authenticated
@@ -16,4 +42,5 @@ class MicroBlogModelView(sqla.ModelView):
         return redirect(url_for('login', next=request.url))
 
 
-admin.add_view(MicroBlogModelView(Product, db.session))
+admin.add_view(MicroBlogModelView(User, db.session))
+# admin.add_view(MicroBlogModelView(Post, db.session))
